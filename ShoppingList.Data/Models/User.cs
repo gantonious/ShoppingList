42eaae630af182eca1b8ea09556567880490f903
George Antonious <george@antonious.ca>
2018-08-04 11:54:00 -0600

    Add data project

---
+++ ShoppingList.Data/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace ShoppingList.Data.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
